e9bf01b1104116cac6bc82ee79ee4d5cd33de5e0
zohiros <zohir96os@gmail.com>
2024-01-19 12:54:28 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let modeBTN = document.querySelector(".dark-mode button");
+let swtiched = document.querySelector(".switch");
+let body = document.querySelector("body");
+let cards = document.querySelectorAll(".card");
+
+function darkMode() {
+  swtiched.classList.toggle("switchDark");
+  body.classList.toggle("darkMode");
+  cards.forEach((card) => {
+    card.classList.toggle("dark-card");
+  });
+  const isDarkMode = swtiched.classList.contains("switchDark");
+  localStorage.setItem("darkMode", isDarkMode);
+}
+if (localStorage.getItem("darkMode") === "true") {
+  darkMode();
+}
+
+modeBTN.addEventListener("click", darkMode);
